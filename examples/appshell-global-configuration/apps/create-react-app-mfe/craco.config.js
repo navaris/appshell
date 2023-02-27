fbb4d26dce37fd928d67bbf10fccad8b8e035c84
Robert Hamilton <rhamilton@navaris.com>
2023-02-27 05:51:01 -0800

    feat: example mfe app producing global appshell manifest

---
+++ examples/appshell-global-configuration/apps/create-react-app-mfe/craco.config.js
@@ -0,0 +1,40 @@
+const {
+  AppshellManifestPlugin,
+} = require("@navaris/appshell-manifest-webpack-plugin");
+const { container } = require("webpack");
+const pkg = require("../../package.json");
+
+module.exports = {
+  eslint: null,
+  webpack: {
+    configure: (webpackConfig, arg) => {
+      webpackConfig.output.publicPath = "auto";
+      webpackConfig.plugins.push(
+        new container.ModuleFederationPlugin({
+          exposes: {
+            "./App": "./src/App",
+          },
+          filename: "remoteEntry.js",
+          name: "CraModule",
+          shared: {
+            react: {
+              singleton: true,
+              requiredVersion: pkg.dependencies["react"],
+            },
+            "react-dom": {
+              singleton: true,
+              requiredVersion: pkg.dependencies["react-dom"],
+            },
+          },
+        })
+      );
+      webpackConfig.plugins.push(
+        new AppshellManifestPlugin({
+          configsDir: process.env.APPSHELL_CONFIGS_DIR,
+        })
+      );
+
+      return webpackConfig;
+    },
+  },
+};
---
+++ examples/appshell-global-configuration/apps/vanilla-react-mfe/webpack.config.js
@@ -0,0 +1,61 @@
+const { AppshellManifestPlugin } = require('@navaris/appshell-manifest-webpack-plugin');
+const { container } = require('webpack');
+const pkg = require('../../package.json');
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  entry: './src/App',
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3002,
+  },
+  output: {
+    publicPath: 'auto',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-typescript'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new container.ModuleFederationPlugin({
+      exposes: {
+        './Vanilla': './src/App',
+      },
+      filename: 'remoteEntry.js',
+      name: 'VanillaModule',
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: pkg.dependencies['react'],
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: pkg.dependencies['react-dom'],
+        },
+      },
+    }),
+    new AppshellManifestPlugin({
+      configsDir: process.env.APPSHELL_CONFIGS_DIR,
+    }),
+  ],
+};
