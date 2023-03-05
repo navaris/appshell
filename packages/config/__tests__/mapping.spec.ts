ceb55c8f6deed27ad11cff841609b1656269a921
Robert Hamilton <rhamilton@navaris.com>
2023-03-05 07:33:08 -0800

    fix: add shareScope support to appshell remotes

--- packages/config/__tests__/mapping.spec.ts
+++ packages/config/__tests__/mapping.spec.ts
@@ -1,27 +1,47 @@
-import { values } from 'lodash';
+import { entries, values } from 'lodash';
 import configmap from '../src/configmap';
 import { toAppshellManifest } from '../src/mappers';
-import { AppshellConfig } from '../src/types';
+import { AppshellConfig, AppshellManifest } from '../src/types';
 import config from './assets/appshell.config.json';
 
 describe('mapping configurations to domain objects', () => {
-  it('should match snapshot', () => {
-    const APPS_TEST_URL = 'http://remote-endpoint.com';
+  const APPS_TEST_URL = 'http://remote-endpoint.com';
+
+  let appshellManifest: AppshellManifest;
+  beforeEach(() => {
     process.env.APPS_TEST_URL = APPS_TEST_URL;
     const configMap = configmap.create(config as AppshellConfig);
-    const appshellConfig = toAppshellManifest(config as AppshellConfig, configMap);
+    appshellManifest = toAppshellManifest(config as AppshellConfig, configMap);
+  });
 
-    expect(appshellConfig).toMatchSnapshot();
+  it('should match snapshot', () => {
+    expect(appshellManifest).toMatchSnapshot();
   });
 
   it('should replace url placeholders with environment variables', () => {
-    const APPS_TEST_URL = 'http://remote-endpoint.com';
-    process.env.APPS_TEST_URL = APPS_TEST_URL;
-    const configMap = configmap.create(config as AppshellConfig);
-    const appshellConfig = toAppshellManifest(config as AppshellConfig, configMap);
-
     expect(
-      values(appshellConfig.remotes).some((remote) => remote.url.includes(APPS_TEST_URL)),
+      values(appshellManifest.remotes).some((remote) => remote.url.includes(APPS_TEST_URL)),
     ).toBeTruthy();
+  });
+
+  it('should create the scope value from the remote key', () => {
+    expect(
+      entries(appshellManifest.remotes).every(([key, remote]) => key.includes(remote.scope)),
+    ).toBeTruthy();
+  });
+
+  it('should create the module value from the remote key', () => {
+    expect(
+      entries(appshellManifest.remotes).every(([key, remote]) => {
+        const moduleName = key.replace(/\/.+/, '');
+        const moduleKey = key.replace(moduleName, '.');
+
+        return moduleKey === remote.module;
+      }),
+    ).toBeTruthy();
+  });
+
+  it('should map shareScope to manifest remote', () => {
+    expect(values(appshellManifest.remotes).every((remote) => remote.shareScope)).toBeTruthy();
   });
 });
